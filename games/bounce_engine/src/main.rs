c3e3ec2fd02f2a13a25c7d9829592387f9ab149f
hwn0809 <hwn0809@gmail.com>
2025-10-28 15:58:30 +0900

    add gravity / bouncing

--- games/bounce_engine/src/main.rs
+++ games/bounce_engine/src/main.rs
@@ -32,20 +32,63 @@ fn setup_camera(mut commands: Commands) {
 }
 
 fn spawn_balls(mut commands: Commands) {
-    commands.spawn((
-        Sprite {
-            custom_size: Some(Vec2::new(12., 12.)),
-            color: Color::Srgba(CYAN_400),
-            ..default()
-        },
-        Ball,
-    ));
+    use rand::Rng;
+
+    let mut rng = rand::rng();
+
+    for _ in 0..12 {
+        let x = rng.random_range(-420.0..420.0);
+        let y = rng.random_range(50.0..220.0);
+        let vx = rng.random_range(-120.0..120.0);
+        let vy = rng.random_range(20.0..180.0);
+
+        commands.spawn((
+            Sprite {
+                custom_size: Some(Vec2::splat(24.0)),
+                color: Color::Srgba(CYAN_400),
+                ..default()
+            },
+            Ball,
+            CircleCollider::new(12.0),
+            Transform::from_xyz(x, y, 0.0),
+            Velocity::with_drag(Vec2::new(vx, vy), 0.05),
+        ));
+    }
 }
 
-fn apply_gravity() {
-    info!("Gravity");
+fn apply_gravity(mut q: Query<&mut Velocity, With<Ball>>, g: Res<Gravity>, time: Res<Time>) {
+    let dt = time.delta_secs();
+
+    for mut v in &mut q {
+        v.lin_vel += g.0 * dt;
+    }
 }
 
-fn bounce_off_walls() {
-    info!("Bounce");
+fn bounce_off_walls(
+    bounds: Res<Bounds>,
+    mut q: Query<(&mut Transform, &mut Velocity, &CircleCollider), With<Ball>>,
+) {
+    for (mut t, mut v, c) in &mut q {
+        let r = c.radius;
+        let (min_x, max_x) = (-bounds.half_w + r, bounds.half_w - r);
+        let (min_y, max_y) = (-bounds.half_h + r, bounds.half_h - r);
+
+        // FOR X
+        if t.translation.x < min_x {
+            t.translation.x = min_x; // clamp to x-boundary
+            v.lin_vel.x = -v.lin_vel.x * 0.9; // bounce back
+        } else if t.translation.x > max_x {
+            t.translation.x = max_x;
+            v.lin_vel.x = -v.lin_vel.x * 0.9;
+        }
+
+        // FOR Y
+        if t.translation.y < min_y {
+            t.translation.y = min_y;
+            v.lin_vel.y = -v.lin_vel.y * 0.8; // a bit softer  -> gradually decreases the bounce
+        } else if t.translation.y > max_y {
+            t.translation.y = max_y;
+            v.lin_vel.y = -v.lin_vel.y * 0.9;
+        }
+    }
 }
