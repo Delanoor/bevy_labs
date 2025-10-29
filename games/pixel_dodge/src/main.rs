a0f67adafb67b46d2f45279784b40c5534c6f43e
hwn0809 <hwn0809@gmail.com>
2025-10-29 18:05:00 +0900

    add obstacle/falling objects spawning

--- games/pixel_dodge/src/main.rs
+++ games/pixel_dodge/src/main.rs
@@ -1,13 +1,20 @@
-use bevy::{color::palettes::tailwind::RED_300, prelude::*};
-use core_engine::prelude::{CircleCollider, CorePlugin, Health, Velocity};
+use std::time::Duration;
 
+use bevy::{
+    color::palettes::tailwind::{BLUE_300, RED_300},
+    prelude::*,
+};
+use core_engine::prelude::{CircleCollider, CorePlugin, Health, Lifetime, Velocity};
+
+const HALF_W: f32 = 480.0;
+const HALF_H: f32 = 270.0;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(CorePlugin)
         .insert_resource(Bounds {
-            half_w: 480.0,
-            half_h: 270.0,
+            half_w: HALF_W,
+            half_h: HALF_H,
         })
         .insert_resource(SpawnTuning::default())
         .add_systems(Startup, (setup_camera, spawn_player))
@@ -52,17 +59,96 @@ fn spawn_player(mut commands: Commands) {
         Sprite {
             custom_size: Some(Vec2::splat(24.0)),
             color: Color::Srgba(RED_300),
+
             ..default()
         },
         Health::new(1.0),
-        Velocity::default(),
+        Velocity::with_drag(Vec2::ZERO, 0.8),
         CircleCollider::new(12.0),
+        Transform::from_xyz(0.0, -HALF_H, 0.0),
         Player,
     ));
 }
 
-fn player_input(kb: Res<ButtonInput<KeyCode>>, mut q: Query<&mut Velocity, With<Player>>) {}
-fn clamp_player() {}
-fn spawn_hazards() {}
+fn player_input(kb: Res<ButtonInput<KeyCode>>, mut q: Query<&mut Velocity, With<Player>>) {
+    if let Ok(mut v) = q.single_mut() {
+        let mut dir = Vec2::ZERO;
+
+        if kb.pressed(KeyCode::KeyA) || kb.pressed(KeyCode::ArrowLeft) {
+            dir.x -= 1.0;
+        }
+
+        if kb.pressed(KeyCode::KeyD) || kb.pressed(KeyCode::ArrowRight) {
+            dir.x += 1.0;
+        }
+
+        let speed = 300.0;
+
+        // v.lin_vel = if dir == Vec2::ZERO {
+        //     Vec2::ZERO
+        // } else {
+        //     dir.normalize() * speed
+        // };
+
+        // only apply when theres a directional change
+        if dir != Vec2::ZERO {
+            v.lin_vel = dir.normalize() * speed
+        };
+    }
+}
+fn clamp_player(
+    bounds: Res<Bounds>,
+    mut q: Query<(&mut Transform, &mut Velocity, &CircleCollider), With<Player>>,
+) {
+    for (mut t, mut v, c) in q.iter_mut() {
+        let r = c.radius;
+
+        let (min_x, max_x) = (-bounds.half_w + r, bounds.half_w - r);
+
+        // if the player touches
+        if t.translation.x < min_x {
+            t.translation.x = min_x;
+            v.lin_vel.x *= -1.0;
+        } else if t.translation.x > max_x {
+            t.translation.x = max_x;
+            v.lin_vel.x *= -1.0;
+        }
+    }
+}
+fn spawn_hazards(
+    mut commands: Commands,
+    time: Res<Time>,
+    bounds: Res<Bounds>,
+    mut tune: ResMut<SpawnTuning>,
+) {
+    // speeding up spawn over time, min 0.25s
+    tune.timer.tick(time.delta());
+    if tune.timer.just_finished() {
+        use rand::Rng;
+        let mut rng = rand::rng();
+        let x = rng.random_range(-bounds.half_w + 16.0..bounds.half_w - 16.0);
+
+        commands.spawn((
+            Hazard,
+            Velocity::new(Vec2::new(0.0, tune.fall_speed)),
+            CircleCollider::new(12.0),
+            Lifetime::seconds(6.0),
+            Transform::from_xyz(x, bounds.half_h + 20.0, 0.0),
+            Sprite {
+                custom_size: Some(Vec2::splat(20.0)),
+                color: Color::Srgba(BLUE_300),
+
+                ..default()
+            },
+        ));
+
+        let s = tune.timer.duration().as_secs_f32();
+        let new_s = (s * 0.97).max(0.25);
+        tune.timer.set_duration(Duration::from_secs_f32(new_s));
+
+        // increase fall speed
+        tune.fall_speed -= 5.0;
+    }
+}
 
 fn end_if_collision() {}
