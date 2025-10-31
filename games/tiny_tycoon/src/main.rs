4471b36855df8317a816a0b1804149cf6c4d67f1
hwn0809 <hwn0809@gmail.com>
2025-10-31 17:15:14 +0900

    add basic purchase btns

--- games/tiny_tycoon/src/main.rs
+++ games/tiny_tycoon/src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use bevy::{asset, prelude::*};
+use bevy::prelude::*;
 use core_engine::prelude::*;
 use serde::{Deserialize, Serialize};
 
@@ -13,6 +13,7 @@ fn main() {
             1.0,
             TimerMode::Repeating,
         )))
+        .insert_resource(SavePath("saves/tycoon.ron".into()))
         .add_systems(Startup, (spawn_camera, spawn_ui, try_load_then_reconstruct))
         .add_systems(
             Update,
@@ -27,7 +28,9 @@ fn main() {
         .run();
 }
 
-fn spawn_camera() {}
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct SaveFile {
@@ -47,7 +50,7 @@ pub struct FactorySave {
     pub pos: (f32, f32),
 }
 
-#[derive(Serialize, Deserialize, Clone, Copy)]
+#[derive(Serialize, Deserialize, Clone, Copy, Debug)]
 pub enum FactoryKind {
     Basic,
     Advanced,
@@ -77,6 +80,8 @@ pub struct Produces {
     pub per_second: f32,
 }
 
+// =========== UI Components ===============
+
 #[derive(Component)]
 pub struct UiMoneyText;
 #[derive(Component)]
@@ -86,7 +91,7 @@ pub struct BtnBuyFactory(FactoryKind);
 #[derive(Component)]
 pub struct BtnUpgradeFactory;
 
-fn spawn_basic_factory(mut commands: Commands, pos: Vec2, asset_server: Res<AssetServer>) {
+fn spawn_basic_factory(mut commands: Commands, pos: Vec2, asset_server: &AssetServer) {
     let sprite = asset_server.load("cat_3.png");
     commands.spawn((
         Factory {
@@ -94,15 +99,16 @@ fn spawn_basic_factory(mut commands: Commands, pos: Vec2, asset_server: Res<Asse
             level: 1,
         },
         Produces {
-            item: ItemId((1)),
+            item: ItemId(1),
             per_second: 1.0, // 1 item / sec
         },
         Sprite {
             image: sprite,
-            custom_size: Some(Vec2::splat(20.0)),
+            custom_size: Some(Vec2::new(691.0 / 10.0, 563.0 / 10.0)),
             image_mode: SpriteImageMode::Scale(ScalingMode::FillCenter),
             ..default()
         },
+        Transform::from_xyz(pos.x, pos.y, 0.0),
     ));
 }
 
@@ -122,10 +128,277 @@ fn tick_production(
     }
 }
 
-fn spawn_ui() {}
-fn try_load_then_reconstruct() {}
+fn spawn_ui(mut commands: Commands) {
+    commands
+        .spawn((
+            Node {
+                position_type: PositionType::Absolute,
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                padding: UiRect::all(Val::Px(40.0)),
+                ..default()
+            },
+            BackgroundColor(Color::srgb(0.0, 0.0, 0.0).with_alpha(0.92)),
+        ))
+        .with_children(|parent| {
+            // Buy Basic Factory button
+            parent.spawn((
+                Button,
+                Node {
+                    width: Val::Px(320.0),
+                    height: Val::Px(60.0),
+                    flex_direction: FlexDirection::Column,
+                    justify_content: JustifyContent::Center,
+                    margin: UiRect::bottom(Val::Px(10.0)),
+                    ..default()
+                },
+                Text::new("BUY NORMAL FACTORY"),
+                TextFont {
+                    font_size: 24.0,
+                    ..default()
+                },
+                TextColor(Color::srgb(0.85, 0.30, 0.0)),
+                BtnBuyFactory(FactoryKind::Basic),
+            ));
 
-fn update_ui() {}
-fn click_buy_factory() {}
-fn click_upgrade_factory() {}
-fn autosave_every_5s_dev() {}
+            // Buy Advanced Factory button
+            parent.spawn((
+                Button,
+                Node {
+                    width: Val::Px(320.0),
+                    height: Val::Px(60.0),
+                    flex_direction: FlexDirection::Column,
+                    justify_content: JustifyContent::Center,
+                    margin: UiRect::bottom(Val::Px(10.0)),
+                    ..default()
+                },
+                Text::new("BUY ADVANCED FACTORY"),
+                TextFont {
+                    font_size: 24.0,
+                    ..default()
+                },
+                TextColor(Color::srgb(0.30, 0.60, 0.85)),
+                BtnUpgradeFactory,
+            ));
+
+            // Upgrade Factory button
+            parent.spawn((
+                Button,
+                Node {
+                    width: Val::Px(320.0),
+                    height: Val::Px(60.0),
+                    flex_direction: FlexDirection::Column,
+                    justify_content: JustifyContent::Center,
+                    margin: UiRect::bottom(Val::Px(10.0)),
+                    ..default()
+                },
+                Text::new("UPGRADE FACTORY"),
+                TextFont {
+                    font_size: 24.0,
+                    ..default()
+                },
+                TextColor(Color::srgb(0.30, 0.85, 0.30)),
+                BtnUpgradeFactory,
+            ));
+
+            // Money display
+            parent.spawn((
+                Node {
+                    width: Val::Px(320.0),
+                    height: Val::Px(40.0),
+                    flex_direction: FlexDirection::Column,
+                    justify_content: JustifyContent::Center,
+                    margin: UiRect::top(Val::Px(20.0)),
+                    ..default()
+                },
+                Text::new("$ 0"),
+                TextFont {
+                    font_size: 28.0,
+                    ..default()
+                },
+                TextColor(Color::srgb(1.0, 1.0, 1.0)),
+                UiMoneyText,
+            ));
+
+            // Inventory display
+            parent.spawn((
+                Node {
+                    width: Val::Px(320.0),
+                    height: Val::Px(40.0),
+                    flex_direction: FlexDirection::Column,
+                    justify_content: JustifyContent::Center,
+                    ..default()
+                },
+                Text::new(""),
+                TextFont {
+                    font_size: 20.0,
+                    ..default()
+                },
+                TextColor(Color::srgb(0.8, 0.8, 0.8)),
+                UiInvText,
+            ));
+        });
+}
+
+fn update_ui(
+    money: Res<Money>,
+    inv: Res<Inventory>,
+    mut text_queries: ParamSet<(
+        Query<&mut Text, With<UiMoneyText>>,
+        Query<&mut Text, With<UiInvText>>,
+    )>,
+) {
+    if money.is_changed() {
+        if let Ok(mut t) = text_queries.p0().single_mut() {
+            **t = format!("$ {:.0}", money.0);
+        }
+    }
+    if inv.is_changed() {
+        if let Ok(mut t) = text_queries.p1().single_mut() {
+            let mut line = String::new();
+            for (id, qty) in inv.0.iter().take(3) {
+                line.push_str(&format!("I{}: {} ", id.0, qty));
+            }
+            **t = line;
+        }
+    }
+}
+fn click_buy_factory(
+    mut commands: Commands,
+    mut money: ResMut<Money>,
+    asset_server: Res<AssetServer>,
+    mut counter: Local<u32>,
+    factories: Query<&Factory>,
+    q: Query<(&Interaction, &BtnBuyFactory), Changed<Interaction>>,
+) {
+    // Initialize counter based on existing factories (on first run after load)
+    if *counter == 0 && !factories.is_empty() {
+        *counter = factories.iter().count() as u32;
+    }
+
+    for (interaction, btn) in &q {
+        if *interaction == Interaction::Pressed {
+            let cost = match btn.0 {
+                FactoryKind::Basic => 50.0,
+                FactoryKind::Advanced => 200.0,
+            };
+
+            if money.0 >= cost {
+                money.0 -= cost;
+
+                // Spawn factory at varied position using counter
+                *counter += 1;
+                let angle = (*counter as f32) * 0.7; // Spiral-like pattern
+                let radius = (*counter as f32) * 2.0;
+                let x = angle.cos() * radius;
+                let y = angle.sin() * radius;
+
+                spawn_basic_factory(commands.reborrow(), Vec2::new(x, y), &asset_server);
+
+                println!("Bought {:?} factory for ${}", btn.0, cost);
+            }
+        }
+    }
+}
+fn click_upgrade_factory(
+    mut money: ResMut<Money>,
+    mut factories: Query<(&mut Factory, &mut Produces)>,
+    q_btn: Query<&Interaction, (Changed<Interaction>, With<BtnUpgradeFactory>)>,
+) {
+    for interaction in &q_btn {
+        if *interaction == Interaction::Pressed {
+            // Upgrade the first factory found
+            if let Some((mut factory, mut produces)) = factories.iter_mut().next() {
+                let cost = (factory.level as f32) * 100.0;
+
+                if money.0 >= cost {
+                    money.0 -= cost;
+                    factory.level += 1;
+                    produces.per_second *= 1.5; // 50% increase per level
+
+                    println!("Upgraded factory to level {}", factory.level);
+                }
+            }
+        }
+    }
+}
+fn autosave_every_5s_dev(
+    time: Res<Time>,
+    mut t: Local<Timer>,
+    money: Res<Money>,
+    inv: Res<Inventory>,
+    q: Query<(&Factory, &Transform)>,
+    path: Res<SavePath>,
+) {
+    if t.duration().is_zero() {
+        *t = Timer::from_seconds(5.0, TimerMode::Repeating)
+    }
+    t.tick(time.delta());
+    if !t.just_finished() {
+        return;
+    }
+
+    use ron::ser::{PrettyConfig, to_string_pretty};
+    let factories = q
+        .iter()
+        .map(|(f, tr)| FactorySave {
+            kind: f.kind,
+            level: f.level,
+            pos: (tr.translation.x, tr.translation.y),
+        })
+        .collect::<Vec<_>>();
+
+    let sf = SaveFile {
+        money: money.0,
+        inventory: inv.0.iter().map(|(k, v)| (*k, *v)).collect(),
+        factories,
+        last_real_secs: time.elapsed_secs_f64(),
+    };
+
+    let ron = to_string_pretty(&sf, PrettyConfig::default()).expect("serialize");
+    std::fs::create_dir_all(path.0.parent().unwrap()).ok();
+    std::fs::write(&path.0, ron).expect("write save");
+}
+
+// fn init_game_or_load(mut commands: Commands, asset_time: Res<Time>) {
+//     commands.insert_resource(SavePath(std::path::PathBuf::from("saves/tycoon.ron")));
+//     // defaults
+//     commands.insert_resource(Money(100.0));
+//     commands.insert_resource(Inventory::default());
+// }
+
+fn try_load_then_reconstruct(
+    mut commands: Commands,
+    time: Res<Time>,
+    path: Res<SavePath>,
+    mut money: ResMut<Money>,
+    mut inv: ResMut<Inventory>,
+    asset_server: Res<AssetServer>,
+) {
+    if let Ok(text) = std::fs::read_to_string(&path.0) {
+        if let Ok(sf) = ron::from_str::<SaveFile>(&text) {
+            money.0 = sf.money;
+
+            inv.0 = sf.inventory.into_iter().collect();
+
+            // reconstruct factories
+            for f in sf.factories {
+                spawn_basic_factory(
+                    commands.reborrow(),
+                    Vec2::new(f.pos.0, f.pos.1),
+                    &asset_server,
+                );
+            }
+
+            // offline catchup
+            let elapsed = (time.elapsed_secs_f64() - sf.last_real_secs).max(0.0);
+            let offline_ticks = (elapsed / 1.0).floor() as u32;
+            if offline_ticks > 0 {
+
+                // TODO credits
+            }
+        }
+    }
+}
